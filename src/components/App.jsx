84e8a3b8fc5d25c00af1c97eecffef900f14cd25
Maryna-smile <maryna.naidych@gmail.com>
2022-12-29 11:57:21 +0200

    Rewrote to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,115 @@
-// import { LoginForm } from './Form/Form';
-
+import { useState } from 'react';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
-import { Component } from 'react';
 import { Section } from './Section/Section';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState (0);
+  const [neutral, setNeutral] = useState (0);
+  const [bad, setBad] = useState (0);
+
+  const increment = e => {
+    console.log(e.target.id)
+    switch (e.target.id) {
+    case 'good':
+      setGood(good + 1);
+      break;
+    case 'neutral':
+      setNeutral(neutral +1 );
+      break;
+    case 'bad':
+      setBad(bad + 1);
+      break;
+      default:
+      return
+    }
+  }
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
   };
 
-  increment = e => {
-    const id = e.currentTarget.id;
-    this.setState(prevstate => {
-      return {
-        [id]: prevstate[id] + 1,
-      };
-    });
-  };
-
-  countTotalFeedback = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
-  };
-
-  countPositiveFeedbackPercentage = () => {
+  const countPositiveFeedbackPercentage = () => {
     return Math.round(
-      ((this.countTotalFeedback() - (this.state.neutral + this.state.bad)) /
-        this.countTotalFeedback()) *
+      ((countTotalFeedback() - (neutral + bad)) /
+        countTotalFeedback()) *
         100
     );
   };
 
-  render() {
+ 
     return (
       <>
-        <Section title={' Please leave feedback'}>
+        <Section title={'Please leave feedback'}>
           <FeedbackOptions
-            options={Object.keys(this.state)}
-            increment={this.increment}
+            options={{good, neutral, bad}}
+            increment={increment}
           />
         </Section>
         <Section title={'Statistics'}>
           <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={this.countTotalFeedback}
-            positivePercentage={this.countPositiveFeedbackPercentage}
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback}
+            positivePercentage={countPositiveFeedbackPercentage}
           />
         </Section>
       </>
     );
   }
-}
+
+
+
+
+
+// export class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   increment = e => {
+//     const id = e.currentTarget.id;
+//     this.setState(prevstate => {
+//       return {
+//         [id]: prevstate[id] + 1,
+//       };
+//     });
+//   };
+
+//   countTotalFeedback = () => {
+//     return this.state.good + this.state.neutral + this.state.bad;
+//   };
+
+//   countPositiveFeedbackPercentage = () => {
+//     return Math.round(
+//       ((this.countTotalFeedback() - (this.state.neutral + this.state.bad)) /
+//         this.countTotalFeedback()) *
+//         100
+//     );
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <Section title={' Please leave feedback'}>
+//           <FeedbackOptions
+//             options={Object.keys(this.state)}
+//             increment={this.increment}
+//           />
+//         </Section>
+//         <Section title={'Statistics'}>
+//           <Statistics
+//             good={this.state.good}
+//             neutral={this.state.neutral}
+//             bad={this.state.bad}
+//             total={this.countTotalFeedback}
+//             positivePercentage={this.countPositiveFeedbackPercentage}
+//           />
+//         </Section>
+//       </>
+//     );
+//   }
+// }
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,19 +1,27 @@
+import PropTypes from 'prop-types';
 import css from './FeedbackOptions.module.css';
 
 export const FeedbackOptions = ({ options, increment }) => {
+  const buttonOptions = Object.keys(options);
   return (
     <div>
-      {options.map(el => (
+      {buttonOptions.map(el => (
         <button
           className={css.button}
           key={el}
           id={el}
           type="button"
           onClick={increment}
+          
         >
           {el}
         </button>
       ))}
     </div>
   );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.objectOf(PropTypes.number),
+  increment: PropTypes.func,
 };
